2612c4c2583a4d81ae0b1bb85baf3c6392616f55
DemahomGitHub <demahom.toure@gmail.com>
2015-11-21 10:03:14 +0100

    Base de données et Application web
    Cours de base de données avancées et applications web.

---
+++ LaboBD21Novembre/LaboBD21Novembre/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboBD21Novembre
+{
+    public class Customer
+    {
+        public long Id { get; set; }
+        public double AccountBalance { get; set; }
+        public string AdresseLine1 { get; set; }
+        public string AdresseLine2 { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string PosteCode { get; set; }
+        public string Remark { get; set; }
+    }
+}
